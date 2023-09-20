853be046db035cbbf9b253c3886dcd75429b674c
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-09-20 20:23:32 -0300

    add: criar, obter

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -18,12 +18,26 @@ namespace WEBAPI_AgendamentoTarefa.Controllers
         {
             _context = context;
         }
-        [HttpPost]
-        public IActionResult Create(Tarefa tarefa)
+        [HttpGet()]
+        public IActionResult Obter()
         {
-            _context.Add(tarefa);
-            _context.SaveChanges();
-            return Ok();
+            return Ok(_context.Tarefas);
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Tarefa tarefa)
+        {
+            var contextBanco = _context.Tarefas.Find(tarefa.Id);
+            if (contextBanco == null)
+            {
+                if (tarefa.Data == DateTime.MinValue)
+                    return BadRequest(new { Erro = "A data da tarefa não pode ser vazia." });
+                _context.Add(tarefa);
+                _context.SaveChanges();
+                return Ok();
+                // return CreatedAtAction(nameof());
+            }
+            return BadRequest(new { Erro = "Úsuario já existente." });
         }
     }
 }
