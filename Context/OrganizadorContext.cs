874aca9d8bc945c3bb480eefaac192bd42452278
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-09-20 20:12:09 -0300

    add: adicionando migrations e ajustando controller

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WEBAPI_AgendamentoTarefa.Controllers;
+using WEBAPI_AgendamentoTarefa.Models;
 
 namespace WEBAPI_AgendamentoTarefa.Context
 {
-    public class OrganizadorContext
+    public class OrganizadorContext : DbContext
     {
-        
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options) { }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
     }
 }
--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -2,11 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WEBAPI_AgendamentoTarefa.Context;
+using WEBAPI_AgendamentoTarefa.Models;
 
 namespace WEBAPI_AgendamentoTarefa.Controllers
 {
-    public class TarefaController
+    [ApiController]
+    [Route("[controller]")]
+    public class TarefaController : ControllerBase
     {
-        
+        private readonly OrganizadorContext _context;
+
+        public TarefaController(OrganizadorContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult Create(Tarefa tarefa)
+        {
+            _context.Add(tarefa);
+            _context.SaveChanges();
+            return Ok();
+        }
     }
 }
